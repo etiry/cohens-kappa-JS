f65a956e32855631a6cd05c0ca9b62466bc474f0
Aaron Norby <aaron.norby@gmail.com>
2015-04-19 21:47:00 -0400

    Some documentation for kappa_script.R in comments.

--- kappa_script.R
+++ kappa_script.R
@@ -2,21 +2,28 @@ library(irr)
 library(jsonlite)
 
 # Example using numeric categories. 
+
+# Convert to lists from JSON strings. (Note: must be STRINGS.)
 rev1 <- fromJSON('{"Rambo":5,"Point Break":3,"Speed":2,"Aliens":5,"Red Dawn":3,"Blond on Blond":4,"Terminator":3}')
 rev2 <- fromJSON('{"Terminator":5,"Speed":3,"Aliens":5,"Point Break":2,"Red Dawn":2,"Blond on Blond":1,"Rambo":3}')
 
+# Convert to column vectors.
 rev1c <- cbind(rev1)
 rev2c <- cbind(rev2)
 
+# Create a data frame for each rater. 
 dat1 <- as.data.frame(rev1c)
 dat2 <- as.data.frame(rev2c)
 
+# Merge data frames into one, by subject (e.g., "Rambo"). 
 total <- merge(dat1, dat2, by="row.names")
 total$rev1 <- unlist(total$rev1)
 total$rev2 <- unlist(total$rev2)
 
+# Leave off the first column which contains subject names ("Rambo").  
 data <- total[,c(2,3)]
 
+# Invoke the function from irr library. 
 kU <- kappa2(data, "unweighted")
 kL <- kappa2(data, "equal")
 kS <- kappa2(data, "squared")
